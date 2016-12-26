57199fc016333126d96e67e991bc48e9dde19a24
Derek <derekmohrdesign@gmail.com>
2016-12-26 00:50:58 -0500

    wp-captions added, template for site docs

---
+++ wp-content/themes/onemohrtime/page-docs.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Half-Page
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package onemohrtime
+ */
+
+get_header(); ?>
+    
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+            
+			<?php while (have_posts()): the_post();
+				get_template_part( 'template-parts/content-half', 'page' );
+			endwhile; ?>
+            
+		</main><!-- #main -->
+	</div><!-- #primary -->
+    
+<?php get_footer();
