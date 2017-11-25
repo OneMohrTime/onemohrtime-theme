ba71fad840e84d707ceab212bfbd37ffb3c44cd2
Derek <derekmohrdesign@gmail.com>
2017-11-25 14:47:06 -0500

    switch ninja id's

--- wp-content/themes/onemohrtime/templates/page-contact.php
+++ wp-content/themes/onemohrtime/templates/page-contact.php
@@ -4,6 +4,6 @@ $contact = get_post('24');
 ?>
 
 <section id="contact" class="site__contact">
-	<?php echo do_shortcode('[ninja_form id=1]'); // localhost ?>
-	<?php //echo do_shortcode('[ninja_form id=3]'); // production ?>
+	<?php //echo do_shortcode('[ninja_form id=1]'); // localhost ?>
+	<?php echo do_shortcode('[ninja_form id=3]'); // production ?>
 </section>
