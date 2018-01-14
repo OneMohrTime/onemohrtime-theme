9032948b8abb129e8d054adfc382a8555a4897a4
Derek Mohr <dmohr@designvox.com>
2018-01-13 23:40:58 -0500

    mobile header fix

--- wp-content/themes/onemohrtime/assets/scripts/main.js
+++ wp-content/themes/onemohrtime/assets/scripts/main.js
@@ -128,25 +128,26 @@ $(document).ready(function() {
     });
     
     // Text Rotator
-    
-    $('.rotate').each(function() {
-        var el = $(this);
-        var text = $(this).html().split(",");
-        el.html(text[0]);
-        setInterval(function () {
-            el.animate({
-                textShadowBlur : 20,
-                opacity : 0
-            }, 500, function () {
-                index = $.inArray(el.html(), text);
-                if ((index + 1) == text.length) index = -1;
-                el.text(text[index + 1]).animate({
-                    textShadowBlur : 0,
-                    opacity : 1
-                }, 500);
-            });
-        }, 2000);
-    });
+	if(window.matchMedia('(min-width: 840px)').matches) {
+		$('.rotate').each(function() {
+			var el = $(this);
+			var text = $(this).html().split(",");
+			el.html(text[0]);
+			setInterval(function () {
+				el.animate({
+					textShadowBlur : 20,
+					opacity : 0
+				}, 500, function () {
+					index = $.inArray(el.html(), text);
+					if ((index + 1) == text.length) index = -1;
+					el.text(text[index + 1]).animate({
+						textShadowBlur : 0,
+						opacity : 1
+					}, 500);
+				});
+			}, 2000);
+		});
+	}
 	
 	// Sticky-kit
 	
