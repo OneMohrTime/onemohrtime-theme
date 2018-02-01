bc3ada59b88d0f991c2dd2f01a3b535192c2d5f2
OneMohrTime <derek@onemohrti.me>
2018-01-31 20:30:03 -0500

    analytics through WP plugin

--- wp-content/themes/onemohrtime/templates/head.php
+++ wp-content/themes/onemohrtime/templates/head.php
@@ -1,18 +1,8 @@
 <head>
 	
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-19790921-2"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag('js', new Date());
-		
-		gtag('config', 'UA-19790921-2');
-	</script>
-	
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta http-equiv="x-ua-compatible" content="ie=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
 	
     <link rel="profile" href="http://gmpg.org/xfn/11" />
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
