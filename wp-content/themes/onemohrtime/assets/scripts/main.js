c870b5a82d12ba23dee3e58202d9d132dc0ef62d
Derek Mohr <dmohr@designvox.com>
2018-02-21 20:58:55 -0500

    instagram update

--- wp-content/themes/onemohrtime/assets/scripts/main.js
+++ wp-content/themes/onemohrtime/assets/scripts/main.js
@@ -244,7 +244,7 @@ $(document).ready(function() {
 		success : function(data) {
 //			console.log(data);
 			for( x in data.data ) {
-				$('#latest_instagram').append('<figure class="widget__instagram--image"><img src="' + data.data[x].images.thumbnail.url + '" alt="' + data.data[x].caption.text + '" title="' + data.data[x].caption.text + '" srcset="' + data.data[x].images.low_resolution.url + ' 306w, ' + data.data[x].images.standard_resolution.url + '" /><a href="' + data.data[x].link +'" target="_blank" class="widget__instagram--link"></a></figure>');
+				$('#latest_instagram').append('<figure class="widget__instagram--image"><img src="' + data.data[x].images.thumbnail.url + '" alt="' + data.data[x].caption.text + '" srcset="' + data.data[x].images.low_resolution.url + ' 306w, ' + data.data[x].images.standard_resolution.url + '" /><figcaption class="widget__instagram--caption"><p>' + data.data[x].caption.text + '</p><a class="btn"><span class="fa fa-instagram"></span> View on Instagram</a></figcaption><a href="' + data.data[x].link +'" title="View on Instagram" target="_blank" class="widget__instagram--link"></a></figure>');
 				// data.data[x].images.thumbnail.url - URL of image 150х150
 				// data.data[x].images.low_resolution.url - URL of image 306x306
 				// data.data[x].images.standard_resolution.url - URL of image 612х612
