61b8f601207d9e38974622ff38d4976fec38c03f
Derek Mohr <dmohr@designvox.com>
2018-03-06 22:54:46 -0500

    remove contact form

--- wp-content/themes/onemohrtime/templates/page-contact.php
+++ wp-content/themes/onemohrtime/templates/page-contact.php
@@ -1,9 +1,11 @@
 <?php 
 
-$contact = get_post('24');
-?>
+// $contact = get_post('24'); ?>
 
 <section id="contact" class="site__contact">
-	<?php //echo do_shortcode('[ninja_form id=1]'); // localhost ?>
-	<?php echo do_shortcode('[ninja_form id=3]'); // production ?>
+	<?php // echo do_shortcode('[ninja_form id=1]'); // localhost ?>
+	<?php // echo do_shortcode('[ninja_form id=3]'); // production ?>
+	<div class="nf-form-cont wysiwyg">
+		<p>I am currently not taking on any new projects, but you can still <a href="mailto:derek@onemohrti.me">email me</a>.</p>
+	</div>
 </section>
