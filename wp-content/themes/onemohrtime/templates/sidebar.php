8bfa082706e1eb64fa4595b8f26fb9d7de6e20d1
OneMohrTime <derek@onemohrti.me>
2018-04-21 15:13:22 -0400

    composer install sage
    v 9.0.0

--- wp-content/themes/onemohrtime/templates/sidebar.php
+++
@@ -1,8 +0,0 @@
-<?php //get_sidebar(); ?>
-
-<div class="widget widget__instagram">
-	<h3 class="widget__instagram--title">Instagram <span><a href="//instagram.com/onemohrtimedesign" target="_blank">@onemohrtimedesign</a></span></h3>
-	<div id="latest_instagram" class="widget__instagram--feed fade-content"></div>
-</div>
-
-<?php //dynamic_sidebar('sidebar-primary'); ?>
