2c63ead64767ed6c442eb8c96fad2241fb74bc67
OneMohrTime <derek@onemohrti.me>
2018-05-06 01:26:01 -0400

    Design page context

--- wp-content/themes/onemohrtime/page-design.php
+++ wp-content/themes/onemohrtime/page-design.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Single page template file
+ * Project page template file
  *
  * @package  WordPress
  * @subpackage  SageTimber
@@ -8,5 +8,24 @@
  */
 
 $context = Timber::get_context();
+$args = array(
+	// Get post type project
+	'post_type' => 'design',
+	// Get all posts
+	'posts_per_page' => -1,
+	// Gest post by "graphic" category
+//	'meta_query' => array(
+//		array(
+//			'key' => 'project_category',
+//			'value' => 'graphic',
+//			'compare' => 'LIKE'
+//		)
+//	),
+	// Order by post date
+	'orderby' => array(
+		'date' => 'DESC'
+	)
+);
+$context['gallery'] = Timber::get_posts( $args );
 
 Timber::render('pages/page-design.twig', $context);
