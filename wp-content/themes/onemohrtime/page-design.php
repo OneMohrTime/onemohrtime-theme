9822e4805115c69fac578750630077a5b1415c17
OneMohrTime <derek@onemohrti.me>
2018-05-20 15:57:49 -0400

    rebase

--- wp-content/themes/onemohrtime/page-design.php
+++ wp-content/themes/onemohrtime/page-design.php
@@ -9,20 +9,9 @@
 
 $context = Timber::get_context();
 $args = array(
-//	 Get post type project
-	'post_type' => 'design',
-	// Get all posts
+	'post_type'      => 'design',
 	'posts_per_page' => -1,
-	// Gest post by "graphic" category
-//	'meta_query' => array(
-//		array(
-//			'key' => 'project_category',
-//			'value' => 'graphic',
-//			'compare' => 'LIKE'
-//		)
-//	),
-	// Order by post date
-	'orderby' => array(
+	'orderby'        => array(
 		'date' => 'DESC'
 	)
 );
