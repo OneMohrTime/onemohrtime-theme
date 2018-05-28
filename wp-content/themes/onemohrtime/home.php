7b5e80695325a055c53dbfdb4d3d3a7001b5979d
OneMohrTime <derek@onemohrti.me>
2018-05-28 02:54:26 -0400

    Posts, project, more tweaks

---
+++ wp-content/themes/onemohrtime/home.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The main template file
+ *
+ * @package  WordPress
+ * @subpackage  SageTimber
+ * @since  SageTimber 0.1
+ */
+
+$context = Timber::get_context();
+$args = array(
+	'post_type'      => 'post',
+	'posts_per_page' => -1,
+	'orderby'        => array(
+		'date' => 'DESC'
+	)
+);
+$context['posts'] = Timber::get_posts( $args );
+Timber::render( 'pages/home.twig', $context );
--- wp-content/themes/onemohrtime/index.php
+++ wp-content/themes/onemohrtime/index.php
@@ -10,7 +10,7 @@
 $context = Timber::get_context();
 $context['posts'] = Timber::get_posts();
 $templates = array( 'pages/index.twig' );
-if ( is_home() ) {
-	array_unshift( $templates, 'pages/home.twig' );
-}
+//if ( is_home() ) {
+//	array_unshift( $templates, 'pages/home.twig' );
+//}
 Timber::render( $templates, $context );
