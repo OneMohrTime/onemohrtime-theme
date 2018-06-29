bf36e0a5f9a6bf365c86f9065092aea3681a7b49
Derek Mohr <dmohr@designvox.com>
2018-06-29 16:04:19 -0400

    organization, working on sliding elements

--- wp-content/themes/onemohrtime/assets/scripts/main.js
+++ wp-content/themes/onemohrtime/assets/scripts/main.js
@@ -65,42 +65,105 @@
 					}
 				});
 				
-				// Mobile Multi Step Accordion Menu
-				// https://codepen.io/juanbrujo/pen/repMZj
-//				if (window.matchmedia('(max-width: 600px)').matches) {
-					var subNavTrigger = $('.multilevel-link');
-					subNavTrigger.on('click', function(e){
-						e.preventDefault();
-						// if already active
-						if( $(this).hasClass('active') ) {
-							$(this).removeClass('active').next('ul'); // .slideUp()
-							$(this).parent('li').siblings('li'); 	  // .slideDown()
-						} else {
-							// if inactive
-							if( $(this).siblings('ul').length ) {
-								$(this).addClass('active').next('ul'); // .slideDown()
-								$(this).parent('li').siblings('li');   // .slideUp()
-							}
-						}
-					});
-//				}
+				// Show an element
+				var show = function (elem) {
+					// Get the natural height of the element
+					var getHeight = function () {
+						elem.style.display = 'block'; // Make it visible
+						var height = elem.scrollHeight + 'px'; // Get it's height
+						elem.style.display = ''; //  Hide it again
+						return height;
+					};
+					
+					var height = getHeight(); // Get the natural height
+					elem.classList.add('is-visible'); // Make the element visible
+					elem.style.height = height; // Update the max-height
+					
+					// Once the transition is complete, remove the inline max-height so the content can scale responsively
+					window.setTimeout(function () {
+						elem.style.height = '';
+					}, 350);
+				};
 				
-				// custom lazyload TODO
-				function lazyLoadImages() {
-					var st = $(window).scrollTop();
-					$('img.lazy').each(function () {
-						var img = $(this);
-						if (img.attr('src')) return;
-						if (img.offset().top < $(window).height() + st) {
-							img.attr('src', img.data('src'));
-						}
-					});
-				}
-				if ($('img.lazy').length > 0) {
-					$(window).scroll(function () {
-						lazyLoadImages();
-					});
-				}
+				// Hide an element
+				var hide = function (elem) {
+					// Give the element a height to change from
+					elem.style.height = elem.scrollHeight + 'px';
+					
+					// Set the height back to 0
+					window.setTimeout(function () {
+						elem.style.height = '0';
+					}, 1);
+					
+					// When the transition is complete, hide it
+					window.setTimeout(function () {
+						elem.classList.remove('is-visible');
+					}, 350);
+				};
+				
+				// Toggle element visibility
+				var toggle = function (elem, timing) {
+					// If the element is visible, hide it
+					if (elem.classList.contains('is-visible')) {
+						hide(elem);
+						return;
+					}
+					
+					// Otherwise, show it
+					show(elem);
+					
+				};
+				
+				// Listen for click events
+				document.addEventListener('click', function (event) {
+					// Make sure clicked element is our toggle
+					if (!event.target.classList.contains('toggle')) return;
+					
+					// Prevent default link behavior
+					event.preventDefault();
+					
+					// Get the content
+					var content = document.querySelector(event.target.hash);
+					if (!content) return;
+					
+					// Toggle the content
+					toggle(content);
+				}, false);
+				
+//				// Mobile Multi Step Accordion Menu
+//				// https://codepen.io/juanbrujo/pen/repMZj
+//				var subNavTrigger = $('.multilevel-link');
+//				subNavTrigger.on('click', function(e){
+//					e.preventDefault();
+//					// if already active
+//					if( $(this).hasClass('active') ) {
+//						$(this).removeClass('active').next('ul'); // .slideUp()
+//						$(this).parent('li').siblings('li'); 	  // .slideDown()
+//					} else {
+//						// if inactive
+//						if( $(this).siblings('ul').length ) {
+//							$(this).addClass('active').next('ul'); // .slideDown()
+//							$(this).parent('li').siblings('li');   // .slideUp()
+//						}
+//					}
+//				});
+				
+//				// custom lazyload TODO
+//				function lazyLoadImages() {
+//					var st = $(window).scrollTop();
+//					$('img.lazy').each(function () {
+//						var img = $(this);
+//						if (img.attr('src')) return;
+//						if (img.offset().top < $(window).height() + st) {
+//							img.attr('src', img.data('src'));
+//						}
+//					});
+//				}
+//				if ($('img.lazy').length > 0) {
+//					$(window).scroll(function () {
+//						lazyLoadImages();
+//					});
+//				}
 				
 				// Fade in content
 				// Hide direct children of .fade-content element
