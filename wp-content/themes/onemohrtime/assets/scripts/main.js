f42e2cad7cc88a56bcb998e7cd323ebf01f2fd8f
Derek Mohr <dmohr@designvox.com>
2018-07-10 14:54:00 -0400

    remove bourbon parallax

--- wp-content/themes/onemohrtime/assets/scripts/main.js
+++ wp-content/themes/onemohrtime/assets/scripts/main.js
@@ -329,39 +329,6 @@
 					}
 				});
 				
-				// Bourbon Refills parallax effect
-				// refills.bourbon.io/components#parallax
-				function parallax() {
-					if ($('#js-parallax-window').length > 0) {
-						var plxBackground = $('#js-parallax-background');
-						var plxWindow     = $('#js-parallax-window');
-						
-						var plxWindowTopToPageTop   = $(plxWindow).offset().top;
-						var windowTopToPageTop      = $(window).scrollTop();
-						var plxWindowTopToWindowTop = plxWindowTopToPageTop - windowTopToPageTop;
-						
-						var plxBackgroundTopToPageTop      = $(plxBackground).offset().top;
-						var windowInnerHeight              = window.innerHeight;
-						var plxBackgroundTopToWindowTop    = plxBackgroundTopToPageTop - windowTopToPageTop;
-						var plxBackgroundTopToWindowBottom = windowInnerHeight - plxBackgroundTopToWindowTop;
-						var plxSpeed                       = 0.35;
-						
-						plxBackground.css('top', -(plxWindowTopToWindowTop * plxSpeed) + 'px');
-					}
-				}
-				
-				$(document).ready(function () {
-					if ($('#js-parallax-window').length) {
-						parallax();
-					}
-				});
-				
-				$(window).scroll(function (e) {
-					if ($('#js-parallax-window').length) {
-						parallax();
-					}
-				});
-				
 				// Fancybox
 				$('[data-fancybox]').fancybox({
 					// Enable infinite gallery navigation
