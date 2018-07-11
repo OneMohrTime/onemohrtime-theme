ebb956d147cc075339a82104ac7e96e8763cd67b
Derek Mohr <dmohr@designvox.com>
2018-07-11 12:31:58 -0400

    single.twig updated to show multiple templates

--- wp-content/themes/onemohrtime/single.php
+++ wp-content/themes/onemohrtime/single.php
@@ -1,12 +1,23 @@
 <?php
 /**
- * Single page template
+ * The Template for displaying all single posts (taken from timber-starter-theme)
  *
  * @package  WordPress
- * @subpackage  SageTimber
- * @since  SageTimber 0.1
+ * @subpackage  Timber
+ * @since    Timber 0.1
  */
 
 $context = Timber::get_context();
+$post    = Timber::query_post();
+//$post    = new TimberPost();
 
-Timber::render('pages/single.twig', $context);
+$context['post']     = $post;
+$context['banner']   = get_field('project_header');
+$context['details']  = get_field('project_deets');
+$context['sections'] = get_field('project_specs');
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'single-' . $post->ID . '.twig', 'single-' . $post->post_type . '.twig', 'single.twig' ), $context );
+}
