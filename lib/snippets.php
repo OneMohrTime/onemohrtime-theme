c0d9f1f62129cd63780db2e090d7254208713286
Derek Mohr <dmohr@designvox.com>
2018-07-23 16:46:46 -0400

    Add $data to Timber

--- lib/snippets.php
+++ lib/snippets.php
@@ -3,7 +3,6 @@
 /**
  * Move Yoast to bottom
  */
-
 function yoasttobottom() {
 	return 'low';
 }
@@ -12,7 +11,6 @@ add_filter('wpseo_metabox_prio', 'yoasttobottom');
 /**
  * Add webp to mime type
  */
-
 function webp_upload_mimes( $existing_mimes ) {
 	// add webp to the list of mime types
 	$existing_mimes['webp'] = 'image/webp';
@@ -23,19 +21,17 @@ function webp_upload_mimes( $existing_mimes ) {
 add_filter( 'mime_types', 'webp_upload_mimes' );
 
 /**
- * Add page excerpts
+ * Add excerpts to Page
  */
-
 add_post_type_support( 'page', 'excerpt' );
 
 /**
  * Enqueue Font Awesome.
  */
-
-add_action( 'wp_enqueue_scripts', 'custom_load_font_awesome' );
 function custom_load_font_awesome() {
     wp_enqueue_script( 'font-awesome', '//use.fontawesome.com/releases/v5.1.0/js/all.js', array(), null );
 }
+add_action( 'wp_enqueue_scripts', 'custom_load_font_awesome' );
 
 /**
  * Filter the HTML script tag of `font-awesome` script to add `defer` attribute.
@@ -45,11 +41,20 @@ function custom_load_font_awesome() {
  *
  * @return   Filtered HTML script tag.
  */
-
-add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
 function add_defer_attribute( $tag, $handle ) {
     if ( 'font-awesome' === $handle ) {
         $tag = str_replace( ' src', ' defer src', $tag );
     }
     return $tag;
 }
+add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
+
+/**
+ * Add $data to Timber
+ */
+function add_to_context($data){
+    // this is where you can add your own data to Timber's context object
+    $data['foo'] = 'bar';
+    return $data;
+}
+add_filter('timber_context', 'add_to_context');
