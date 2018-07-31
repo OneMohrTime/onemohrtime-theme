355ee261f95d8ce8a31b1c529ac39a081df5932a
OneMohrTime <derek@onemohrti.me>
2018-07-30 23:44:55 -0400

    Simplify page & single templates

--- page.php
+++ page.php
@@ -8,6 +8,16 @@
  */
 
 $context = Timber::get_context();
-$post = new TimberPost();
+$post    = new TimberPost();
 
-Timber::render('pages/page.twig', $context);
+$projects = array(
+	'post_type'      => 'design',
+	'posts_per_page' => -1,
+	'orderby'        => 'menu_order',
+	'order'          => 'ASC'
+);
+$context['post']    = $post;
+$context['gallery'] = Timber::get_posts( $projects );
+$context['roles']   = new TimberTerm('design');
+
+Timber::render( array( 'pages/page-' . $post->post_name . '.twig', 'pages/page.twig' ), $context );
--- single.php
+++ single.php
@@ -8,5 +8,16 @@
  */
 
 $context = Timber::get_context();
+//$post  = new TimberPost();
+$post    = Timber::query_post();
 
-Timber::render('pages/single.twig', $context);
+$context['post']     = $post;
+$context['banner']   = get_field('project_header');
+$context['details']  = get_field('project_deets');
+$context['sections'] = get_field('project_specs');
+
+if ( post_password_required( $post->ID ) ) {
+	Timber::render( 'single-password.twig', $context );
+} else {
+	Timber::render( array( 'pages/single-' . $post->ID . '.twig', 'pages/single-' . $post->post_type . '.twig', 'pages/single.twig' ), $context );
+}
