cd5bfb806654bb0e710302a77fe8658e83ce6e06
OneMohrTime <derek@onemohrti.me>
2018-08-12 22:31:12 -0400

    Change design page to custom grid

--- page.php
+++ page.php
@@ -10,15 +10,17 @@
 $context = Timber::get_context();
 $post    = new TimberPost();
 
-$projects = array(
-	'post_type'      => 'design',
-	'posts_per_page' => -1,
-	'orderby'        => 'menu_order',
-	'order'          => 'ASC'
-);
-$context['post']    = $post;
-$context['gallery'] = Timber::get_posts( $projects );
-$context['roles']   = new TimberTerm('design');
+$projects  = get_field('project_grid');
+//$projects2 = array(
+//	'post_type'      => 'design',
+//	'posts_per_page' => -1,
+//	'orderby'        => 'menu_order',
+//	'order'          => 'ASC'
+//);
+$context['post']     = $post;
+//$context['gallery']  = Timber::get_posts( $projects2 );
+$context['projects']  = Timber::get_posts( $projects );
+$context['roles']    = new TimberTerm('design');
 $context['image_grid'] = get_field('image_grid');
 
 Timber::render( array( 'pages/page-' . $post->post_name . '.twig', 'pages/page.twig' ), $context );
