72b9053cd193e87029d7f968ade120edce6f4549
OneMohrTime <derek@onemohrti.me>
2018-09-19 21:08:16 -0400

    More fading and parallax

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -108,71 +108,6 @@
 				// add .get-faded class
 				$(wpGallery).addClass('get-faded');
 				
-				// Show an element
-				var show = function (elem) {
-					// Get the natural height of the element
-					var getHeight = function () {
-						elem.style.display = 'block'; // Make it visible
-						var height = elem.scrollHeight + 'px'; // Get it's height
-						elem.style.display = ''; //  Hide it again
-						return height;
-					};
-					
-					var height = getHeight(); // Get the natural height
-					elem.classList.add('is-visible'); // Make the element visible
-					elem.style.height = height; // Update the max-height
-					
-					// Once the transition is complete, remove the inline max-height so the content can scale responsively
-					window.setTimeout(function () {
-						elem.style.height = '';
-					}, 350);
-				};
-				
-				// Hide an element
-				var hide = function (elem) {
-					// Give the element a height to change from
-					elem.style.height = elem.scrollHeight + 'px';
-					
-					// Set the height back to 0
-					window.setTimeout(function () {
-						elem.style.height = '0';
-					}, 1);
-					
-					// When the transition is complete, hide it
-					window.setTimeout(function () {
-						elem.classList.remove('is-visible');
-					}, 350);
-				};
-				
-				// Toggle element visibility
-				var toggle = function (elem, timing) {
-					// If the element is visible, hide it
-					if (elem.classList.contains('is-visible')) {
-						hide(elem);
-						return;
-					}
-					
-					// Otherwise, show it
-					show(elem);
-					
-				};
-				
-				// Listen for click events
-				document.addEventListener('click', function (event) {
-					// Make sure clicked element is our toggle
-					if (!event.target.classList.contains('toggle')) return;
-					
-					// Prevent default link behavior
-					event.preventDefault();
-					
-					// Get the content
-					var content = document.querySelector(event.target.hash);
-					if (!content) return;
-					
-					// Toggle the content
-					toggle(content);
-				}, false);
-				
 				// Slide nav menu up and down
 				// Initial scroll position
 				var scrollState = 0;
@@ -418,6 +353,10 @@
 							slideShadows : false,
 						}
 					});
+					var titles = [];
+					$('#home_banner .swiper-slide').each(function(i) {
+						titles.push($(this).attr('data-title'))
+					});
 					var homeBanner = new Swiper('#home_banner', {
 						initialSlide    : 1, // start in the middle (should be 0 - 2)
 						slidesPerView   : 'auto',
@@ -434,7 +373,7 @@
 							el           : '#home_banner .swiper-pagination',
 							clickable    : true,
 							renderBullet : function (index, className) {
-								return '<span class="' + className + ' swiper-pagination-bullet--long"><span class="screen-reader-text">' + (index + 1) + '</span></span>';
+								return '<span class="' + className + ' swiper-pagination-bullet--long"><span class="swiper-pagination-bullet__title">' + titles[index] + '</span><span class="screen-reader-text">Slide ' + (index + 1) + '</span></span>';
 							}
 						},
 						navigation : {
