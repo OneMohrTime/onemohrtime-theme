04b0477589fba1bd6daddbfefe6251b4a837383e
Derek Mohr <derek@mightyinthemidwest.com>
2020-10-10 15:59:35 -0400

    [#15] Move assets/js to assets/scripts

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+
+// ==========================================================================
+// Main javascript file
+// ==========================================================================
+
+//
+// Used to import, check, and initiate functions used globally amongst elements
+//
+
+// Import Polyfills
+// import './polyfills/includes';
+
+// Import Util
+// import './util/includes';
+
+// Import Components
+import stickyNavigation from './components/stickyNavigation';
+
+// Initialize Scripts
+stickyNavigation();
