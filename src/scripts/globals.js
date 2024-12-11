21cb5a2347f3358ed757bad405aa28ea826d283b
Derek Mohr <derek@onemohrti.me>
2024-12-10 22:27:09 -0500

    Create tooltips with Floating UI

--- src/scripts/globals.js
+++ src/scripts/globals.js
@@ -5,7 +5,7 @@
 
 // Import dependencies
 // =============================================================================
-// import npm from 'npm';
+import { computePosition, flip, shift, offset } from '@floating-ui/dom';
 
 // Set default function
 // =============================================================================
@@ -28,6 +28,51 @@ export default function() {
         // Smooth scroll to the target element
         targetElement.scrollIntoView({ behavior: 'smooth' });
       }
+    });
+  });
+
+  /**
+   * Floating UI tooltips for multiple elements
+   */
+
+  const tooltips = document.querySelectorAll('.c-tooltip');
+  const tooltipAnchors = document.querySelectorAll('.c-tooltip__anchor');
+
+  tooltipAnchors.forEach((tooltipAnchor, index) => {
+    const tooltip = tooltips[index]; // Match each anchor with its tooltip
+
+    function update() {
+      computePosition(tooltipAnchor, tooltip, {
+        placement: 'bottom',
+        middleware: [
+          offset(6),
+          flip(),
+          shift({ padding: 5 }),
+        ],
+      }).then(({ x, y }) => {
+        Object.assign(tooltip.style, {
+          left: `${x}px`,
+          top: `${y}px`,
+        });
+      });
+    }
+
+    function showTooltip() {
+      tooltip.style.display = 'block';
+      update();
+    }
+
+    function hideTooltip() {
+      tooltip.style.display = '';
+    }
+
+    [
+      ['mouseenter', showTooltip],
+      ['mouseleave', hideTooltip],
+      ['focus', showTooltip],
+      ['blur', hideTooltip],
+    ].forEach(([event, listener]) => {
+      tooltipAnchor.addEventListener(event, listener);
     });
   });
 
