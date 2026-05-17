ab2d13f061358e8cc3a74a95b0bc451e8ba85a20
Derek Mohr <derek@onemohrti.me>
2026-05-17 13:59:12 -0400

    Redesign footer

--- src/scripts/modules.js
+++ src/scripts/modules.js
@@ -11,7 +11,7 @@ export { default as CustomCursor } from './modules/CustomCursor';
 export { default as DrawSVG } from './modules/DrawSVG';
 // export { default as Example } from './modules/Example';
 export { default as Fancybox } from './modules/Fancybox';
-// export { default as FitText } from './modules/FitText';
+export { default as FitText } from './modules/FitText';
 export { default as KeyboardMenu } from './modules/KeyboardMenu';
 export { default as Load } from './modules/Load';
 export { default as Marquee } from './modules/Marquee';
